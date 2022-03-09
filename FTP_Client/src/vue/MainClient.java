8624383c5a09311cc25d46aa89e0a6f713f04336
Arthurc0 <chaff.arthur@gmail.com>
2022-03-09 15:31:39 +0100

    Début nouvelle ihm

--- FTP_Client/src/vue/MainClient.java
+++ FTP_Client/src/vue/MainClient.java
@@ -5,34 +5,46 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JSplitPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.ImageIcon;
-import java.awt.Color;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Toolkit;
+import javax.swing.JTree;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+
+import javax.swing.JMenuItem;
 
 public class MainClient extends JFrame {
 	private static final long serialVersionUID = 1L;
+
 	private JPanel contentPane;
-	private JScrollPane scrollTexte;
-	private static JTextArea txtCommandes;
-	private static JTextField txtCommande;
-	private static JButton btnEnvoi;
-	private ArrayList<String> historique;
-	private int idHistorique = 0;
+	private JLabel lblClient;
+	private JLabel lblServeur;
+	private JSplitPane splitClientServeur;
+	private JScrollPane scrollClient;
+	private JScrollPane scrollServeur;
+	private JScrollPane scrollLog;
+	private JTextArea txtLog;
+	private JTree treeClient;
+	private JTree treeServeur;
+	private JPopupMenu menuClient;
+	private JMenuItem mntmClientEnvoyerFichier;
+	private JMenuItem mntmClientActualiser;
+	private JPopupMenu menuServeur;
+	private JMenuItem mntmServeurCreerDossier;
+	private JMenuItem mntmServeurSupprimerDossier;
+	private JMenuItem mntmServeurTelecharger;
+	private JMenuItem mntmServeurActualiser;
+	private JLabel lblLog;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -40,7 +52,6 @@ public class MainClient extends JFrame {
 				try {
 					MainClient frame = new MainClient();
 					frame.setVisible(true);
-					Traitement.execution(txtCommandes, txtCommande, btnEnvoi);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -49,165 +60,208 @@ public class MainClient extends JFrame {
 	}
 	
 	public MainClient() {
+		setResizable(false);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(MainClient.class.getResource("/vue/icone.png")));
 		setTitle("Client FTP");
-		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 510, 607);
+		setBounds(100, 100, 980, 607);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		contentPane.add(getScrollTexte());
-		contentPane.add(getTxtCommande());
-		contentPane.add(getBtnEnvoi());
-		
-		historique = new ArrayList<String>();
-		
-		addWindowListener(new WindowAdapter() {
-			public void windowOpened( WindowEvent e ) {
-				txtCommande.requestFocus();
+		contentPane.add(getLblClient());
+		contentPane.add(getLblServeur());
+		contentPane.add(getSplitClientServeur());
+		contentPane.add(getScrollLog());
+		contentPane.add(getLblLog());
+	}
+
+	private JLabel getLblClient() {
+		if (lblClient == null) {
+			lblClient = new JLabel("Fichiers du client");
+			lblClient.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			lblClient.setBounds(10, 11, 120, 19);
+		}
+		return lblClient;
+	}
+
+	private JLabel getLblServeur() {
+		if (lblServeur == null) {
+			lblServeur = new JLabel("Fichiers du serveur");
+			lblServeur.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			lblServeur.setBounds(836, 13, 128, 15);
+		}
+		return lblServeur;
+	}
+
+	private JSplitPane getSplitClientServeur() {
+		if (splitClientServeur == null) {
+			splitClientServeur = new JSplitPane();
+			splitClientServeur.setResizeWeight(0.4);
+			splitClientServeur.setBounds(10, 41, 954, 347);
+			splitClientServeur.setLeftComponent(getScrollClient());
+			splitClientServeur.setRightComponent(getScrollServeur());
+		}
+		return splitClientServeur;
+	}
+
+	private JScrollPane getScrollClient() {
+		if (scrollClient == null) {
+			scrollClient = new JScrollPane();
+			scrollClient.setViewportView(getTreeClient());
+		}
+		return scrollClient;
+	}
+
+	private JScrollPane getScrollServeur() {
+		if (scrollServeur == null) {
+			scrollServeur = new JScrollPane();
+			scrollServeur.setViewportView(getTreeServeur());
+		}
+		return scrollServeur;
+	}
+
+	private JScrollPane getScrollLog() {
+		if (scrollLog == null) {
+			scrollLog = new JScrollPane();
+			scrollLog.setBounds(10, 437, 954, 130);
+			scrollLog.setViewportView(getTxtLog());
+		}
+		return scrollLog;
+	}
+
+	private JTextArea getTxtLog() {
+		if (txtLog == null) {
+			txtLog = new JTextArea();
+			txtLog.setEditable(false);
+		}
+		return txtLog;
+	}
+
+	private JTree getTreeClient() {
+		if (treeClient == null) {
+			DefaultMutableTreeNode root = new DefaultMutableTreeNode("root");
+	        addChilds(root, "root");
+			treeClient = new JTree(new DefaultTreeModel(root));
+			addPopup(treeClient, getMenuClient());
+		}
+		return treeClient;
+	}
+
+	private JTree getTreeServeur() {
+		if (treeServeur == null) {
+			treeServeur = new JTree();
+			addPopup(treeServeur, getMenuServeur());
+		}
+		return treeServeur;
+	}
+
+	private JPopupMenu getMenuClient() {
+		if (menuClient == null) {
+			menuClient = new JPopupMenu();
+			menuClient.setLabel("");
+			menuClient.add(getMntmClientEnvoyerFichier());
+			menuClient.add(getMntmClientActualiser());
+		}
+		return menuClient;
+	}
+
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
 			}
 		});
 	}
-	private JScrollPane getScrollTexte() {
-		if (scrollTexte == null) {
-			scrollTexte = new JScrollPane();
-			scrollTexte.setBounds(10, 11, 475, 502);
-			scrollTexte.setViewportView(getTxtCommandes());
+
+	private JMenuItem getMntmClientEnvoyerFichier() {
+		if (mntmClientEnvoyerFichier == null) {
+			mntmClientEnvoyerFichier = new JMenuItem("Envoyer le fichier");
 		}
-		return scrollTexte;
-	}
-	private JTextArea getTxtCommandes() {
-		if (txtCommandes == null) {
-			txtCommandes = new JTextArea();
-			
-			txtCommandes.setWrapStyleWord(true);
-			txtCommandes.setLineWrap(true);
-			txtCommandes.setFont(new Font("Consolas", Font.PLAIN, 15));
-			txtCommandes.setEditable(false);
-		}
-		return txtCommandes;
-	}
-	private JTextField getTxtCommande() {
-		if (txtCommande == null) {
-			txtCommande = new JTextField();
-			
-			txtCommande.addKeyListener(new KeyAdapter() {
-				@Override
-				public void keyReleased(KeyEvent e) {
-					
-					// Parcours des commandes précédentes (historique)
-					if(e.getKeyCode() == KeyEvent.VK_UP) {
-						if(idHistorique > 0) {
-							idHistorique--;
-							txtCommande.setText(historique.get(idHistorique));
-						}
-					} else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-						if(historique.size() > 0 && idHistorique < historique.size()) {
-							idHistorique++;
-							
-							if(idHistorique == historique.size())
-								txtCommande.setText("");
-							else
-								txtCommande.setText(historique.get(idHistorique));
-						}
-					} else if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
-						if(txtCommande.getText().length() == 0)
-							idHistorique = historique.size();
-					}
-					
-					if(envoiPossible()) {
-						if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-							envoyerCommande(txtCommande.getText());
-						}
-					}
-				}
-			});
-			
-			txtCommande.addFocusListener(new FocusAdapter() {
-				@Override
-				public void focusGained(FocusEvent e) {
-					if(txtCommande.getText().equals("Envoyer une commande...")) {
-						txtCommande.setForeground(Color.BLACK);
-						txtCommande.setText("");
-					}
-				}
-				@Override
-				public void focusLost(FocusEvent e) {
-					if(txtCommande.getText().length() == 0) {
-						txtCommande.setForeground(Color.DARK_GRAY);
-						txtCommande.setText("Envoyer une commande...");
-					}
-				}
-			});
-			txtCommande.setText("Envoyer une commande...");
-			txtCommande.setForeground(Color.DARK_GRAY);
-			txtCommande.setFont(new Font("Consolas", Font.PLAIN, 15));
-			txtCommande.setBounds(10, 524, 427, 32);
-			txtCommande.setColumns(10);
-		}
-		return txtCommande;
-	}
-	private JButton getBtnEnvoi() {
-		if (btnEnvoi == null) {
-			btnEnvoi = new JButton("");
-			btnEnvoi.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent arg0) {
-					envoyerCommande(txtCommande.getText());
-				}
-			});
-			btnEnvoi.setToolTipText("Envoyer la commande");
-			btnEnvoi.setEnabled(false);
-			btnEnvoi.setIcon(new ImageIcon(MainClient.class.getResource("/vue/icone_envoi.png")));
-			btnEnvoi.setBounds(447, 524, 38, 32);
-		}
-		return btnEnvoi;
+		return mntmClientEnvoyerFichier;
 	}
 
-	// L'envoi d'une commande est possible lorsque le champ n'est pas vide
-	private boolean envoiPossible() {
-		boolean res = txtCommande.getText().trim().length() != 0;
-		btnEnvoi.setEnabled(res);
-		return res;
+	private JMenuItem getMntmClientActualiser() {
+		if (mntmClientActualiser == null) {
+			mntmClientActualiser = new JMenuItem("Actualiser");
+		}
+		return mntmClientActualiser;
+	}
+
+	private JPopupMenu getMenuServeur() {
+		if (menuServeur == null) {
+			menuServeur = new JPopupMenu();
+			menuServeur.add(getMntmServeurCreerDossier());
+			menuServeur.add(getMntmServeurSupprimerDossier());
+			menuServeur.add(getMntmServeurTelecharger());
+			menuServeur.add(getMntmServeurActualiser());
+		}
+		return menuServeur;
+	}
+
+	private JMenuItem getMntmServeurCreerDossier() {
+		if (mntmServeurCreerDossier == null) {
+			mntmServeurCreerDossier = new JMenuItem("Créer un dossier");
+		}
+		return mntmServeurCreerDossier;
+	}
+
+	private JMenuItem getMntmServeurSupprimerDossier() {
+		if (mntmServeurSupprimerDossier == null) {
+			mntmServeurSupprimerDossier = new JMenuItem("Supprimer le dossier");
+		}
+		return mntmServeurSupprimerDossier;
+	}
+
+	private JMenuItem getMntmServeurTelecharger() {
+		if (mntmServeurTelecharger == null) {
+			mntmServeurTelecharger = new JMenuItem("Télécharger le fichier");
+		}
+		return mntmServeurTelecharger;
+	}
+
+	private JMenuItem getMntmServeurActualiser() {
+		if (mntmServeurActualiser == null) {
+			mntmServeurActualiser = new JMenuItem("Actualiser");
+		}
+		return mntmServeurActualiser;
 	}
 	
-	private void envoyerCommande(String commande) {
-		txtCommande.setText("");
-		envoiPossible();
-		historique.add(commande);
-		idHistorique = historique.size();
-		
-		if(commande.equals("clear")) {
-			txtCommandes.setText("");
-		} else {
-			txtCommandes.append(commande + "\n");
-			
-			if(commande.equals("help")) {
-				txtCommandes.append("Informations concernant l'ensemble des commandes exécutables sur ce logiciel.\n");
-				txtCommandes.append("Un argument entouré de crochets est un argument facultatif.\n\n");
-				
-				txtCommandes.append("- user nom         Envoie le nom de compte du client\n");
-				txtCommandes.append("- pass mdp         Envoie le mot de passe du compte du client\n");
-				txtCommandes.append("- cd [chemin]      Accède au chemin demandé s'il existe\n");
-				txtCommandes.append("- ls [chemin]      Affiche le contenu du dossier demandé s'il existe\n");
-				txtCommandes.append("- pwd              Affiche le chemin relatif au dossier courant\n");
-				txtCommandes.append("- stor fichier     Envoie un fichier présent dans le dossier du client (root), vers le dossier courant du serveur\n");
-				txtCommandes.append("- get fichier      Télécharge un fichier présent dans le dossier courant du serveur, vers le dossier du client (root)\n");
-				txtCommandes.append("- mkdir dossier    Crée le dossier demandé s'il n'existe pas\n");
-				txtCommandes.append("- rmdir dossier    Supprime le dossier demandé s'il est vide\n");
-				txtCommandes.append("- clear            Efface le texte affiché sur la sortie\n\n");
-				txtCommandes.append("- bye              Arrête la communication avec le serveur FTP\n\n");
-			} else {
-				try {
-					Traitement.envoyerCommande(commande);
-				} catch (IOException e) {
-					
-				}
-			}
+	private JLabel getLblLog() {
+		if (lblLog == null) {
+			lblLog = new JLabel("Message du serveur");
+			lblLog.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			lblLog.setBounds(10, 407, 141, 19);
 		}
-		
-		// Auto scroll
-		txtCommandes.setCaretPosition(txtCommandes.getDocument().getLength());
+		return lblLog;
 	}
+	
+	
+	
+	
+	
+    private void addChilds(DefaultMutableTreeNode rootNode, String path) {
+        File[] files = new File(path).listFiles();
+        for(File file:files) {
+            if(file.isDirectory()) {
+                DefaultMutableTreeNode subDirectory = new DefaultMutableTreeNode(file.getName());
+                addChilds(subDirectory, file.getAbsolutePath());
+                rootNode.add(subDirectory);
+            } else {
+                rootNode.add(new DefaultMutableTreeNode(file.getName()));
+            }
+        }
+    }
 }
